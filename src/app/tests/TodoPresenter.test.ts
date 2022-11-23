84adb3bf40ed4112574ad4e5c8414063b30e0b07
Jongco <narira0922@gmail.com>
2022-11-23 14:48:55 +0900

    test: write deleteTodo test

--- src/app/tests/TodoPresenter.test.ts
+++ src/app/tests/TodoPresenter.test.ts
@@ -4,6 +4,7 @@ import TodoPresenterImpl from "../TodoPresenter";
 describe("Todo", () => {
   let presenter: TodoPresenter;
   let addTodo;
+  let deleteTodo;
   const todoList: Todo[] = [
     { id: 1, todo: "간지나게 밥먹기", done: false },
     { id: 2, todo: "우아하게 짐싸기", done: true },
@@ -12,6 +13,7 @@ describe("Todo", () => {
   beforeEach(() => {
     presenter = new TodoPresenterImpl(todoList);
     addTodo = jest.fn();
+    deleteTodo = jest.fn();
   });
 
   it("increase todoList length when add todo", () => {
@@ -20,5 +22,12 @@ describe("Todo", () => {
     expect(presenter.todoList.length).toBe(3);
     expect(presenter.todoList[2].done).toBe(false);
     expect(presenter.todoList[2].todo).toBe("피터지게 공부하기");
+  });
+
+  it("decrease todoList length when delete todo", () => {
+    presenter.deleteTodo(1);
+
+    expect(presenter.todoList.length).toBe(1);
+    expect(presenter.todoList[0].todo).toBe("우아하게 짐싸기");
   });
 });
