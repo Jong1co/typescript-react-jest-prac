07a05551e5d4764c638bfc75bd0b8e55acc5132e
Jongco <narira0922@gmail.com>
2022-12-01 19:58:33 +0900

    feat: create  handleChange Method

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import Layout from "../layout/Layout";
 
@@ -11,7 +11,7 @@ type Product = {
 };
 
 const AddProduct = () => {
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState<File>();
   const [product, setProduct] = useState<Product>({
     title: "",
     price: "",
@@ -20,31 +20,59 @@ const AddProduct = () => {
     options: "",
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+  console.log(file, product);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value, files } = e.target;
+    if (name === "file" && files != null) {
+      setFile(files[0]);
+      return;
+    }
+    setProduct((product) => ({ ...product, [name]: value }));
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <Layout>
       <h3>새로운 제품 등록</h3>
-      <AddProductForm
-        action=''
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}>
-        <input
-          type='file'
-          accept='image/*'
-          required
-          onChange={(e) => {
-            console.log(e.target.value);
-          }}
-        />
-        <input type='text' required value={product.title} onChange={handleChange} placeholder='제품명' />
-        <input type='number' required value={product.price} onChange={handleChange} placeholder='가격' />
-        <input type='text' required value={product.category} onChange={handleChange} placeholder='카테고리' />
-        <input type='text' required value={product.description} onChange={handleChange} placeholder='제품 설명' />
+      <AddProductForm action='' onSubmit={handleSubmit}>
+        <input type='file' accept='image/*' name='file' required onChange={handleChange} />
         <input
           type='text'
           required
+          name='title'
+          value={product.title}
+          onChange={handleChange}
+          placeholder='제품명'
+        />
+        <input
+          className='price'
+          type='number'
+          name='price'
+          value={product.price}
+          onChange={handleChange}
+          placeholder='가격'
+        />
+        <input
+          type='text'
+          name='category'
+          value={product.category}
+          onChange={handleChange}
+          placeholder='카테고리'
+        />
+        <input
+          type='text'
+          name='description'
+          value={product.description}
+          onChange={handleChange}
+          placeholder='제품 설명'
+        />
+        <input
+          type='text'
+          name='options'
           value={product.options}
           onChange={handleChange}
           placeholder='옵션들(콤마(,)로 구분)'
